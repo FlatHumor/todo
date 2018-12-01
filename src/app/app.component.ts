fffe8ca49db886ccb77a73b7c3cb5bdeee146f31
FlatHumor <eatenbrains@gmail.com>
2018-12-02 02:08:38 +0300

    last action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,15 @@ export class AppComponent {
   title: string = 'todo';
   model: Model = new Model();
 
-  getName(): string {
+  public getName(): string {
     return this.model.user;
   }
 
-  getTodoItems(): Array<TodoItem> {
+  public getTodoItems(): Array<TodoItem> {
     return this.model.items.filter(item => !item.done);
   }
 
-  addItem(itemDescription: string): void {
+  public addItem(itemDescription: string): void {
     if (itemDescription != "")
       this.model.items.push(new TodoItem(itemDescription, false));
   }
